124dc84cd6b00747a9c5530bf5396af7d9cb5460
jaehyeong <wwh0517@naver.com>
2024-07-04 16:11:47 +0900

    2024-07-04

---
+++ spr07anno_auto/src/main/java/anno1_auto/Sender.java
@@ -0,0 +1,10 @@
+package anno1_auto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sender {
+	public void show() {
+		System.out.println("Sender의 show method 수행");
+	}
+}
