46c8cad7fea6e370178da6c640f8eed8e27353cb
jaehyeong <wwh0517@naver.com>
2024-07-17 17:11:57 +0900

    2024-07-17

--- sprweb10db_dao2/src/main/java/pack/model/MemberDao.java
+++ sprweb10db_dao2/src/main/java/pack/model/MemberDao.java
@@ -13,30 +13,26 @@ import pack.controller.MemberBean;
 
 @Repository
 public class MemberDao extends JdbcDaoSupport {
-	
+
 	@Autowired
 	public MemberDao(DataSource dataSource) {
 		setDataSource(dataSource);
 	}
-	
+
 	// 전체 자료 읽기
 	public List<MemberDto> getMemberList() {
 		String sql = "select * from membertab";
-		
+
 		/*
-		List<MemberDto> list = getJdbcTemplate().query(sql, new RowMapper() {
-			@Override
-			public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
-				MemberDto member = new MemberDto();
-				member.setId(rs.getString("id"));
-				member.setName(rs.getString("name"));
-				member.setPasswd(rs.getString("passwd"));
-				member.setReg_date(rs.getString("reg_date"));
-				return member;
-			}
-		});
-		*/
-		
+		 * List<MemberDto> list = getJdbcTemplate().query(sql, new RowMapper() {
+		 * 
+		 * @Override public Object mapRow(ResultSet rs, int rowNum) throws SQLException
+		 * { MemberDto member = new MemberDto(); member.setId(rs.getString("id"));
+		 * member.setName(rs.getString("name"));
+		 * member.setPasswd(rs.getString("passwd"));
+		 * member.setReg_date(rs.getString("reg_date")); return member; } });
+		 */
+
 		// 람다 표현식 사용
 		List<MemberDto> list = getJdbcTemplate().query(sql, (ResultSet rs, int rowNum) -> {
 			MemberDto member = new MemberDto();
@@ -46,21 +42,47 @@ public class MemberDao extends JdbcDaoSupport {
 			member.setReg_date(rs.getString("reg_date"));
 			return member;
 		});
-		
+
 		return list;
 	}
-	
+
 	// 추가
 	public void insData(MemberBean bean) {
 		String sql = "insert into membertab values(?, ?, ?, now())";
-		
-		Object[] params = {bean.getId(), bean.getName(), bean.getPasswd()};
-		getJdbcTemplate().update(sql, params); 
-		
+
+		Object[] params = { bean.getId(), bean.getName(), bean.getPasswd() };
+		getJdbcTemplate().update(sql, params);
+
 	}
-	
+
+	// 특정 레코드 읽기
+	public MemberDto getMember(String id) {
+		String sql = "select * from membertab where id = ?";
+
+		MemberDto dto = (MemberDto) getJdbcTemplate().queryForObject(sql, new Object[] { id }, new RowMapper() {
+			@Override
+			public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+				MemberDto dto = new MemberDto();
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				dto.setPasswd(rs.getString("passwd"));
+				dto.setReg_date(rs.getString("reg_date"));
+				return dto;
+			}
+		});
+		return dto;
+	}
+
 	// 수정
-	
+	public void upData(MemberBean bean) {
+		String sql = "update membertab set name = ?, passwd = ? where id = ?";
+		getJdbcTemplate().update(sql, new Object[] { bean.getName(), bean.getPasswd(), bean.getId() });
+	}
+
 	// 삭제
-				
+	public void delData(String id) {
+		String sql = "delete from membertab where id = ?";
+		getJdbcTemplate().update(sql, new Object[] { id });
+	}
+
 }
