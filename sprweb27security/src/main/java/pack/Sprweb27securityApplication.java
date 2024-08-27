fd3cae5da2ca66b40ec9853ea898ecdf756b4c30
jaehyeong <wwh0517@naver.com>
2024-08-27 12:09:16 +0900

    2024-08-27

---
+++ sprweb27security/src/main/java/pack/Sprweb27securityApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprweb27securityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprweb27securityApplication.class, args);
+	}
+
+}
