c43d8cc05fe1d6b2094b38695179b4743973c095
jaehyeong <wwh0517@naver.com>
2024-08-29 14:29:52 +0900

    2024-08-29

---
+++ sprweb28security_db/src/main/java/pack/Sprweb28securityDbApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprweb28securityDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprweb28securityDbApplication.class, args);
+	}
+
+}
